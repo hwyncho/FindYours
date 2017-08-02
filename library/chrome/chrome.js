d8d13ed7944b178144ff9606879c6759e59a37a9
hwyn.cho <hwyn.cho@gmail.com>
2017-08-02 20:20:28 +0900

    Add commit

---
+++ library/chrome/chrome.js
@@ -0,0 +1,57 @@
+try {
+    var AG_onLoad = function(func) {
+        if (document.readyState === "complete" || document.readyState === "interactive") { func(); }
+        else if (document.addEventListener)document.addEventListener("DOMContentLoaded", func);
+        else if (document.attachEvent)document.attachEvent("DOMContentLoaded", func)
+    };
+
+    var AG_removeElementById = function(id) {
+        var element = document.getElementById(id);
+        
+        if (element && element.parentNode) {
+            element.parentNode.removeChild(element);
+        }
+    };
+
+    var AG_removeElementBySelector = function(selector) {
+        if (!document.querySelectorAll) { return; }
+        
+        var nodes = document.querySelectorAll(selector);
+        
+        if (nodes) {
+            for (var i = 0; i < nodes.length; i++) {
+                if (nodes[i] && nodes[i].parentNode) {
+                    nodes[i].parentNode.removeChild(nodes[i]);
+                }
+            }
+        }
+    };
+    
+    var AG_each = function(selector, fn) {
+        if (!document.querySelectorAll) { return; }
+        
+        var elements = document.querySelectorAll(selector);
+        
+        for (var i = 0; i < elements.length; i++) {
+            fn(elements[i]);
+        };
+    };
+    
+    var AG_removeParent = function(el, fn) {
+        while (el && el.parentNode) {
+            if (fn(el)) {
+                el.parentNode.removeChild(el);
+
+                return;
+            }
+            
+            el = el.parentNode;
+        }
+    };
+    
+    window.wcs_add = { };
+    window.wcs_do = function() { };
+}
+catch (ex) {
+    console.error('Error executing AG js: ' + ex);
+}
